f051f4a9d9c0ea5b5058c9b8d83c110eb71e98cc
bithighlander <bithighlander@gmail.com>
2024-11-01 16:50:23 -0500

    begin rainbowkit

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,17 +4,23 @@ import { http, createStorage, cookieStorage } from 'wagmi';
 import { mainnet, base } from 'wagmi/chains';
 import { Chain, getDefaultConfig } from '@rainbow-me/rainbowkit';
 
-const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "*Go To Wallet Connect and get Project ID*";
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "YOUR_PROJECT_ID";
 
-const supportedChains: Chain[] = [ mainnet, base ];
+const supportedChains: Chain[] = [mainnet, base];
+
+// Define the transports for each chain
+const transports = {
+    [mainnet.id]: http({ chain: mainnet }),
+    [base.id]: http({ chain: base }),
+};
 
 export const config = getDefaultConfig({
-   appName: "keepkey-template",
-   projectId,
-   chains: supportedChains as any,
-   ssr: true,
-   storage: createStorage({
-    storage: cookieStorage,
-   }),
-  transports: supportedChains.reduce((obj, chain) => ({ ...obj, [chain.id]: http() }), {})
- });
+    appName: "keepkey-template",
+    projectId,
+    chains: supportedChains,
+    ssr: true,
+    storage: createStorage({
+        storage: cookieStorage,
+    }),
+    transports,
+});
